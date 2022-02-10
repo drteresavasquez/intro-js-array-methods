7f2e5d00128e994c0fa64a16fd805a510af1aad2
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-02-09 20:19:40 -0600

    added files

---
+++ data/reference.js
@@ -0,0 +1,50 @@
+export const referenceList = [
+  {
+    id: 1,
+    title: "HBCU Info",
+    author: "Wikipedia",
+    price: 0.00,
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Institute_for_Colored_Youth_Building_Historical_Marker_915_Bainbridge_St_Philadelphia_PA_%28DSC_2640%29.jpg/1920px-Institute_for_Colored_Youth_Building_Historical_Marker_915_Bainbridge_St_Philadelphia_PA_%28DSC_2640%29.jpg",
+    youTubeId: "",
+    type: "info",
+    description: "Historically black colleges and universities (HBCUs) are institutions of higher education in the United States that were established before the Civil Rights Act of 1964 with the intention of primarily serving the African-American community. Most of these institutions were founded in the years after the American Civil War and are concentrated in the Southern United States.",
+    externalLink: "https://en.wikipedia.org/wiki/Historically_black_colleges_and_universities",
+    favorite: false
+  },
+  {
+    id: 2,
+    title: "Ted Poston",
+    author: "Mackenzie Lanum",
+    price: 0.00,
+    image: "https://www.blackpast.org/wp-content/uploads/prodimages/files/blackpast_images/Ted_Poston.jpg",
+    youTubeId: "",
+    type: "biography",
+    description: "Ted Poston, the first African American journalist to work at a mainstream newspaper, was born on July 4, 1906, in Hopkinsville, Kentucky.  His mother, Mollie Cox, died when he was ten and he was raised mostly by his eight older siblings, while his father, Ephraim, taught at the Kentucky State Industrial College for Negroes (now Kentucky State University) in Frankfurt, over two hundred miles away.",
+    externalLink: "https://www.blackpast.org/african-american-history/poston-ted-1906-1974/",
+    favorite: true
+  },
+  {
+    id: 3,
+    title: "Essence Festival",
+    author: "NOLA",
+    price: 1000.00,
+    image: "https://s1.ticketm.net/dam/a/45e/32b89f46-6e8a-475f-b827-92795879845e_1198091_TABLET_LANDSCAPE_LARGE_16_9.jpg",
+    youTubeId: "4OfGpj8GXvQ",
+    type: "event",
+    description: 'The Essence Festival, known as "the party with a purpose", is an annual music festival which started in 1995 as a one-time event to celebrate the 25th anniversary of Essence, a magazine aimed primarily towards African-American women. It became the largest African-American culture and music event in the United States.',
+    externalLink: "https://www.essence.com/essence-festival-2022/",
+    favorite: false
+  },
+  {
+    id: 4,
+    title: "Black History Trivia App",
+    author: "Dr. Teresa Vasquez",
+    price: 10.00,
+    image: "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_3000,w_5000,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/shutterstock_1596565837_kde5ec.jpg",
+    youTubeId: "",
+    type: "App",
+    description: "This app will get you excited about Black History! Play the game and get the highest score on the leaderboard.",
+    externalLink: "https://blackhistorytrivia.netlify.app/",
+    favorite: false
+  },
+];
---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+import { card } from "../components/card.js";
+import { referenceList } from "../data/reference.js";
+import { renderToDom } from "../utils/renderToDom.js";
+
+
+// .includes()
+
+// .filter()
+
+// .sort()
+
+// .reduce()
+
+
+
+// ******* SAVE FOR CONNECTING IT ALL TOGETHER ******
+// const startApp = () => {
+//   let refStuff = "";
+//   referenceList.forEach((item) => {
+//     refStuff += card(item);
+//   });
+
+//   renderToDom("#cards", refStuff);
+// }
+// startApp();
