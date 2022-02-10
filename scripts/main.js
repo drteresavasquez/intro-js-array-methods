69157135a437645b61a770c99117132f8815cece
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-02-09 22:00:01 -0600

    rearrange code

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,6 @@ import { card } from "../components/card.js";
 import { referenceList } from "../data/reference.js";
 import { renderToDom } from "../utils/renderToDom.js";
 
-// .includes()
-
-// .filter()
-
-// .reduce()
-
-// .map()
-
-// .findIndex()
-const toggleCart = (event) => {
-  if (event.target.id.includes("fav-btn")) {
-    // GOOGLE: Destructuring assignment
-    const [,id] = event.target.id.split('--');
-
-    const index = referenceList.findIndex(item => item.id === Number(id));
-    referenceList[index].inCart = !referenceList[index].inCart
-    renderCards(referenceList);
-  }
-
-}
-
 // .forEach()
 const renderCards = (array) => {
   let refStuff = "";
@@ -33,11 +12,33 @@ const renderCards = (array) => {
   renderToDom("#cards", refStuff);
 }
 
+// .findIndex() & .includes()
+const toggleCart = (event) => {
+  if (event.target.id.includes("fav-btn")) {
+    // GOOGLE: Destructuring assignment
+    const [,id] = event.target.id.split('--');
+
+    const index = referenceList.findIndex(item => item.id === Number(id));
+    referenceList[index].inCart = !referenceList[index].inCart
+    renderCards(referenceList);
+  }
+}
+
+// .includes()
+
+// .filter()
+
+// .reduce()
+
+// .map()
+
 
 // ******* SAVE FOR CONNECTING IT ALL TOGETHER ******
 const startApp = () => {
+  // PUT ALL CARDS ON THE DOM
   renderCards(referenceList)
 
+  // SELECT THE CARD DIV
   document.querySelector('#cards').addEventListener('click', toggleCart);
 }
 startApp();
