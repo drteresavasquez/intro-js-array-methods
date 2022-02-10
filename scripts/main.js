f32a14b134e02ae2672efbba01d568e5eb4d7ea2
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-02-09 21:56:21 -0600

    solutions

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,6 @@ import { card } from "../components/card.js";
 import { referenceList } from "../data/reference.js";
 import { renderToDom } from "../utils/renderToDom.js";
 
-// .forEach()
-
 // .includes()
 
 // .filter()
@@ -12,14 +10,34 @@ import { renderToDom } from "../utils/renderToDom.js";
 
 // .map()
 
+// .findIndex()
+const toggleCart = (event) => {
+  if (event.target.id.includes("fav-btn")) {
+    // GOOGLE: Destructuring assignment
+    const [,id] = event.target.id.split('--');
 
-// ******* SAVE FOR CONNECTING IT ALL TOGETHER ******
-const startApp = () => {
+    const index = referenceList.findIndex(item => item.id === Number(id));
+    referenceList[index].inCart = !referenceList[index].inCart
+    renderCards(referenceList);
+  }
+
+}
+
+// .forEach()
+const renderCards = (array) => {
   let refStuff = "";
-  referenceList.forEach((item) => {
+  array.forEach((item) => {
     refStuff += card(item);
   });
 
   renderToDom("#cards", refStuff);
+}
+
+
+// ******* SAVE FOR CONNECTING IT ALL TOGETHER ******
+const startApp = () => {
+  renderCards(referenceList)
+
+  document.querySelector('#cards').addEventListener('click', toggleCart);
 }
 startApp();
